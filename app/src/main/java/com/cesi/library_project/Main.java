c459204c06e13016428591fbda9d6b576e933ff2
codlab <codlabtech@gmail.com>
2018-01-30 16:56:32 +0100

    add snippet from eclipse.org

--- app/src/main/java/com/cesi/library_project/Main.java
+++ app/src/main/java/com/cesi/library_project/Main.java
@@ -1,28 +1,28 @@
 package com.cesi.library_project;
 
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.RowLayout;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Text;
 
 public class Main {
 
   public static void main(String[] args) {
-
     Display display = new Display();
-
-    Shell shell = new Shell(display, SWT.RESIZE | SWT.CLOSE | SWT.MIN);
-    shell.setText("Login:");
-
-    RowLayout layout = new RowLayout();
-    layout.wrap = true;
-    shell.setLayout(layout);
-
-    Text txt1 = new Text(shell, SWT.BORDER);
-
-    shell.pack();
+    Image image = display.getSystemImage(SWT.ICON_QUESTION);
+    Shell shell = new Shell(display);
+    shell.setLayout (new GridLayout());
+    Button button = new Button(shell, SWT.PUSH);
+    button.setImage(image);
+    button.setText("Button");
+    shell.setSize(300, 300);
     shell.open();
+    while (!shell.isDisposed ()) {
+      if (!display.readAndDispatch ()) display.sleep ();
+    }
+    display.dispose ();
 
 
     while (!shell.isDisposed()) {
